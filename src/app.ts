a2dcbca57393957df50ab0b5f889822029112bda
Alaa Zamel <alaa.zamel23@gmail.com>
2024-10-12 08:46:59 +0300

    feat: create a middleware for logging and bind it globally

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, {Express} from 'express';
 import router from "./routes/router";
+import {LoggerMiddleware} from "./middlewares/logger.middleware";
 
 const app: Express = express();
 
 app.use(express.json());
+
+// bind the logger middleware
+app.use(LoggerMiddleware)
 
 // bind routes
 app.use(router)
